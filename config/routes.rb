0c96bb68de5f754abab64d9f4905fdd3539f073a
WestKostikov <maksrey@mail.ru>
2020-10-16 16:35:42 +0300

    Change inheritance Elements and route

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
-  resources :elements
   get 'home/index'
   devise_for :authors
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: "home#index"
   scope module: 'authors' do
-    resources :posts
+    resources :posts do
+      resources :elements
+    end
   end
 end
